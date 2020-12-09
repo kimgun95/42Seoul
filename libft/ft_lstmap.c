7d6896d2d3e00f54b005dbbad3c09c05c4fc2b68
kimgun95 <harrison1995@naver.com>
2020-12-09 14:08:18 +0900

    modify lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gukim </var/mail/gukim>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 15:53:57 by gukim             #+#    #+#             */
-/*   Updated: 2020/12/08 16:12:12 by gukim            ###   ########.fr       */
+/*   Updated: 2020/12/09 14:07:50 by gukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,26 @@
 
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list		*res;
-	t_list		*temp;
+	t_list		*res_head;
+	t_list		*res_next;
+	t_list		*curr;
 
-	if (lst == NULL || f == NULL)
+	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	res = NULL;
+	if (!(res_head = ft_lstnew(f(lst->content))))
+		return (NULL);
+	curr = res_head;
+	lst = lst->next;
 	while (lst)
 	{
-		temp = ft_lstnew((*f)(lst->content));
-		if (!temp)
+		if (!(res_next = ft_lstnew(f(lst->content))))
 		{
-			ft_lstclear(&res, del);
+			ft_lstclear(&res_head, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&res, temp);
-		temp = temp->next;
+		curr->next = res_next;
+		curr = res_next;
 		lst = lst->next;
 	}
-	return (res);
+	return (res_head);
 }
